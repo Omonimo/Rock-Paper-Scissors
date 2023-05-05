04c33132b91b65df8d21a5f27c4940ec35e2e235
Omonimo <alfredom.garciag@gmail.com>
2023-05-05 00:53:32 -0600

    code plays one round of rps, displays winner and points

--- rps.js
+++ rps.js
@@ -1,4 +1,4 @@
-function getComputerChoice(){
+ function getComputerChoice(){
     //randomly return scissors paper or rock
     const weapons = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * weapons.length);
@@ -7,25 +7,53 @@ function getComputerChoice(){
 
 }
 
-let currentGame = 0;
-let playerScore = 0;
-let computerScore = 0;
+let playerSelection;
+let computerSelection;
 
-let displayedPlayerScore = document.getElementById("player-score");
-let displayedComputerScore = document.getElementById("computer-score");
-let displayedGame = document.getElementById("current-game");
+const weapons = document.querySelectorAll(".selection")
 
 //get user choice
-function getplayerChoice(){
-    let choice = document.querySelector(`.selection`);
-    let choiceId = choice.id;
-    return choiceId;
+weapons.forEach(weapon => {
+    weapon.addEventListener('click', () => {
 
-}
+        let currentGame = 0;
+        let playerScore = 0;
+        let computerScore = 0;
 
-//display scores
-document.getElementById("player-score").innerHTML = playerScore;
-document.getElementById("computer-score").innerHTML = computerScore;
-document.getElementById("current-game").innerHTML = computerScore;
+        playerSelection = weapon.id;
+        computerSelection = getComputerChoice();
 
-console.log(getplayerChoice());
+        if (computerSelection == playerSelection){
+            result.innerText = "It's a tie";
+            currentGame++;
+        } //player is the winner
+        else if(computerSelection=="rock" && playerSelection == "paper"){
+            playerScore++;
+            result.innerText = "You win! Paper beats Rock";
+            currentGame++;
+        }
+        else if(computerSelection=="paper" && playerSelection == "scissors"){
+            playerScore++;
+            result.innerText = "You win! Scissors beats Paper";
+            currentGame++;
+        }
+        else if(computerSelection=="scissors" && playerSelection == "rock"){
+            playerScore++;
+            result.innerText = "You win! Rock beats Scissors";
+        }
+        //player loses game
+        else {
+            computerScore++;
+            result.innerText =(`You lose ${computerSelection} beats ${playerSelection}`);
+            currentGame++;
+        }
+
+        document.getElementById("player-choice").textContent = playerSelection;
+        document.getElementById("computer-choice").textContent = computerSelection;
+        document.getElementById("player-score").textContent = playerScore;
+        document.getElementById("computer-score").textContent = computerScore;
+        document.getElementById("current-game").textContent = currentGame;
+        
+    })
+});
+
